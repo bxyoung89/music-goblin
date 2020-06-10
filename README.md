2d3701df01ebdf66b4d5d1bf4c794970ae314bd8
Broderick Young <bxyoung89@gmail.com>
2020-06-09 19:36:05 -0500

    made readme and build process.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # music-goblin
 Open source template for making music websites
+
+## Important note on copyright
+
+Don't host things you don't own or don't fall under fair use. That being said, the authors of this repo aren't hosting the files and aren't legally liable for anything you do with it.
+
+## Getting started
+
+Making a new website is easy but first you'll need to get a couple of things
+
+* A [github](https://github.com/) account
+* [Git](https://git-scm.com/)
+* [nodejs](https://nodejs.org/en/)
+
+You'll also want to have the songs you want to use as well.
+
+Basic steps are going to be:
+* fork the repo
+* replace content with your stuff
+* run `npm run build` in the console
+* follow the steps on https://pages.github.com/ to host it
+
+If you want to add more songs, just modify content and run `npm run build` again.
---
+++ build/index.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+
+const getAllFilesInDirectory = (directoryPath) => {
+  const subDirectoriesAndFiles = fs.readdirSync(directoryPath);
+
+  const files = [];
+  subDirectoriesAndFiles.forEach((subDirectoryOrFile) => {
+    const totalPathOfChild = path.join(directoryPath, '/', subDirectoryOrFile);
+    const isDirectory = fs.statSync(totalPathOfChild).isDirectory();
+    if (!isDirectory) {
+      files.push(totalPathOfChild);
+      return;
+    }
+    const subDirectoryFiles = getAllFilesInDirectory(totalPathOfChild);
+    files.push(...subDirectoryFiles);
+  });
+
+  return files;
+};
+
+const allContentFiles = getAllFilesInDirectory(path.join(__dirname, '../content'));
+const allJSONFiles = allContentFiles.filter((file) => file.endsWith('.json'));
+const combinedJSON = allJSONFiles.map((file) => {
+  const fileContent = `${fs.readFileSync(file)}`;
+  try {
+    const parsedJSON = JSON.parse(fileContent);
+    return JSON.stringify(parsedJSON);
+  } catch (e) {
+    console.log(`Could not parse ${file}. Skipping`);
+    return undefined;
+  }
+}).filter((file) => !!file).join(',');
+const contentJSONPath = path.join(__dirname, '../content.json');
+if (fs.existsSync(contentJSONPath)) {
+  fs.unlinkSync(contentJSONPath);
+}
+fs.writeFileSync(contentJSONPath, `[${combinedJSON}]`);
